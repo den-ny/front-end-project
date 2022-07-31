1ee75de1e9e5f77b81230665e0877b2b211b5c61
denny <dennywong@pop-os.localdomain>
2022-07-31 18:59:40 -0400

    updated files

--- catsco/src/App.js
+++ catsco/src/App.js
@@ -1,23 +1,23 @@
 import { Link, Routes, Route } from "react-router-dom";
 import React from "react";
-import Categories from "./Components/Categories";
+import Categories from "./Components/Category";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div>
-      <h1>Catsco!</h1>
+        <h1>Catsco!</h1>
       </div>
       <main>
         <Categories />
-      <Routes>
+        {/* <Routes>
           <Route path="/furniture" element={<Furniture />} />
           <Route path="/food" element={<Food />} />
           <Route path="/toys" element={<Toys />} />
           <Route path="/accessories" element={<Accessories />} />
           <Route path="/litter" element={<Litter />} />
-      </Routes>
+      </Routes> */}
       </main>
     </div>
   );
