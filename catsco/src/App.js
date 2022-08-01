9c5711371e642fbf19719ea8654b54bbc7e8c6ed
Adam Bachrach <nalc1100adam@gmail.com>
2022-07-31 21:49:09 -0400

    start creating a search bar component for the products

--- catsco/src/App.js
+++ catsco/src/App.js
@@ -2,11 +2,13 @@ import { Link, Routes, Route } from "react-router-dom";
 import React from "react";
 import Categories from "./Components/Category";
 import './App.css';
-import Products from './components/products/products';
+import Products from './Components/products/products';
+import SearchBar from "./Components/SearchBar";
 
 const App = () => {
   return (
     <div className="App">
+      <SearchBar />
       <div>
         <Products />
 
