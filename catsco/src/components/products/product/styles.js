b1ce203efbaedd885cbb933939e79dd6a2f49ee1
new owner <newowner@news-MacBook-Air.local>
2022-08-01 09:50:39 -0400

    updated files

--- catsco/src/components/products/product/styles.js
+++ catsco/src/components/products/product/styles.js
@@ -1,8 +1,12 @@
-import { makestyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default makestyles(() => ({
+// const makestyles = () => {
+//   console.log(makeStyles)
+// }
+
+const useStyles = makeStyles({
   root: {
-  maxWidth: '100%',
+    maxWidth: '100%',
   },
   media: {
     height: 0,
@@ -17,4 +21,47 @@ export default makestyles(() => ({
     justifyContent: 'space-between',
   },
 
-}));
+
+
+})
+
+// (() => ({
+
+
+// }));
+
+export default useStyles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { makeStyles } from '@material-ui/core/styles';
+
+// export default makeStyles(() => ({
+//   root: {
+//   maxWidth: '100%',
+//   },
+//   media: {
+//     height: 0,
+//     paddingTop: '56.25%',
+//   },
+//   cardActions: {
+//     display: 'flex',
+//     justifyContent: 'flex-end',
+//   },
+//   cardContent: {
+//     display: 'flex',
+//     justifyContent: 'space-between',
+//   },
+
+// }));
